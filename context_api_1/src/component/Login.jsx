b1c956cbde3446d60d89114f46cd9795b7e48d02
pooja-athane <poojaathane2025@gmail.com>
2026-01-14 15:11:34 +0530

    complete 1st assignment

---
+++ context_api_1/src/component/Login.jsx
@@ -0,0 +1,41 @@
+import React, { useContext, useState } from 'react'
+import UserContext from '../context/UserContext'
+
+function Login() {
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const {setUser} =useContext(UserContext)
+
+    const HandleSubmit = (e) => {
+        e.preventDefault()
+        setUser({username, password})
+        setUsername("")
+        setPassword("")
+    }
+
+    return (
+        <div>
+            <input
+                type='text'
+                placeholder='Enter Username'
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+
+            />
+            <input
+                type='password'
+                placeholder='Enter Your Password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <button
+                onClick={HandleSubmit}>
+                Submit
+            </button>
+        </div>
+    )
+}
+
+export default Login
