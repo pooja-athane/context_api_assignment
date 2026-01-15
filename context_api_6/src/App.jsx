976353592e32294f0282bf3d981f6b34dca1add6
pooja-athane <poojaathane2025@gmail.com>
2026-01-15 09:14:03 +0530

    completed sixth assignment

---
+++ context_api_6/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
+
+function App() {
+  return (
+    <div>
+      <h1>Learning Context Api</h1>
+      <UserContextProvider>
+        <Login/>
+        <Profile/>
+      </UserContextProvider>
+    </div>
+  )
+}
+
+export default App
